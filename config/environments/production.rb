8157eea39ccb9c4f8fb7da6bbd445bd5803928ac
Adam Rainsby <adamrainsby@live.co.uk>
2016-08-10 23:23:44 +0100

    Use sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,16 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
